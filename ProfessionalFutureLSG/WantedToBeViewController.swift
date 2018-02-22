597ae1813a63d382cc4554a0195215b68cf4d0c2
JTorrus <jtorrusgalindo@gmail.com>
2018-02-22 17:48:38 +0100

    Added interaction for segues with prepare function, must fix images not showing issue

--- ProfessionalFutureLSG/WantedToBeViewController.swift
+++ ProfessionalFutureLSG/WantedToBeViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class WantedToBeViewController: UIViewController {
 
+    let professionals = [
+        "devSegue": Professional(title: "Developer", imageName: "dev", salary: "15000", description: "Being a developer is great and fun"),
+        "designerSegue": Professional(title: "UX Designer", imageName: "designer", salary: "7500", description: "A designer makes everything prettier"),
+        "managerSegue": Professional(title: "Project Manager", imageName: "pmanager", salary: "30000", description: "A project manager leads the way to success")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,13 @@ class WantedToBeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let jobDescViewCont = segue.destination as? JobDescriptionViewController {
+            if let segueId = segue.identifier {
+                jobDescViewCont.professionalToDisplay = professionals[segueId]
+            }
+        }
+    }
 }
 
